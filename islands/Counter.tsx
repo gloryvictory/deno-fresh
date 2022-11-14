eaefc9a6d3c6365c95bb73964ada4725da4ae82b
glory <zamaraev@gmail.com>
2022-11-14 22:34:52 +0500

    test to read file

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,17 +1,30 @@
 import { useState } from "preact/hooks";
 import { Button } from "../components/Button.tsx";
+// import fs from 'fs'
 
 interface CounterProps {
   start: number;
 }
 
-export default function Counter(props: CounterProps) {
+export default  function Counter(props: CounterProps) {
   const [count, setCount] = useState(props.start);
+
+  const testJsonFile = async () => {
+  const file1 = await Deno.open("../data/lu.json", { read: true });
+  
+  // let  dataArray = JSON.parse(file1.read())
+  console.log(file1)
+
+  }
+
+
   return (
     <div class="flex gap-2 w-full">
       <p class="flex-grow-1 font-bold text-xl">{count}</p>
       <Button onClick={() => setCount(count - 1)}>-1</Button>
       <Button onClick={() => setCount(count + 1)}>+1</Button>
+      <Button onClick={testJsonFile}>test </Button>
+    
     </div>
   );
 }
